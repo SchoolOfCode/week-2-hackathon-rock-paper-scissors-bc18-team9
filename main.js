1eea368f113ff240d1685a70f3df63d1e07c0959
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-05 15:38:22 +0000

    logic mechanism to compare inputs

--- main.js
+++ main.js
@@ -2,15 +2,35 @@ let userInput = prompt("Welcome to the game! Which would you like to choose? Roc
 let userScore = 0;
 let computerScore = 0;
 
-{
+
     const gameChoices = ['rock', 'paper', 'scissors']
-    let computerInput = Math.floor(Math.random()*gameChoices.length)
+    let computerInput = gameChoices[Math.floor(Math.random()*gameChoices.length)]
     console.log(computerInput)
+
+
+if (userInput === "rock" && computerInput === "paper"){
+    alert("You lose!");
+}
+else if (userInput === "rock" && computerInput === "scissors"){
+    alert("You win!")
+}
+else if (userInput === "paper" && computerInput === "rock"){
+    alert("You win!")
+}
+else if (userInput === "paper" && computerInput === "scissors"){
+    alert("You lose!")
+}
+else if (userInput === "scissors" && computerInput === "rock"){
+    alert("You lose!")
+}   
+else if (userInput === "scissors" && computerInput === "paper"){
+    alert("You win!")
+}  
+else if (userInput === computerInput){
+    alert("It's a tie!")
+}  
+else { 
+    alert(`It's called rock, paper, scissors, not ${userInput}, paper, scissors`)
 }
 
 
-
-// create way to randomly choose computer input
-// create array of choices
-// randomly choose this when the game runs
-// assign to computerInput
