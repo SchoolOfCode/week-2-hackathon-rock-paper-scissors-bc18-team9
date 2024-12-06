6eb282d926bf02bbe86920e7f3d1b8f69dc2b32e
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-06 11:09:07 +0000

    #31 create a number of games played variable and increment

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let userScore = 0;
 let computerScore = 0;
 let playAgain = true;
 
+let gamesLost = 0;
+let gamesWon = 0;
+let gamesTied= 0;
+let gamesPlayed = 0;
+
 alert("Welcome to our Rock, Paper, Scissors Game!");
 
 while (playAgain === true) {
@@ -16,46 +21,53 @@ if (userInput === "rock" && computerInput === "paper"){
     alert("You lose!");
     computerScore++;
     userScore--;
+    gamesLost++;
 }
 else if (userInput === "rock" && computerInput === "scissors"){
     alert("You win!")
     computerScore--;
     userScore++;
+    gamesWon++;
 }
 else if (userInput === "paper" && computerInput === "rock"){
     alert("You win!")
     computerScore--;
     userScore++;
+    gamesWon++;
 }
 else if (userInput === "paper" && computerInput === "scissors"){
     alert("You lose!")
     computerScore++;
     userScore--;
+    gamesLost++;
 }
 else if (userInput === "scissors" && computerInput === "rock"){
     alert("You lose!")
     computerScore++;
     userScore--;
+    gamesLost++;
 }   
 else if (userInput === "scissors" && computerInput === "paper"){
     alert("You win!")
     computerScore--;
     userScore++;
+    gamesWon++;
 }  
 else if (userInput === computerInput){
     alert("It's a tie!")
+    gamesTied++;
 }  
 else { 
     alert(`It's called rock, paper, scissors, not ${userInput}, paper, scissors`)
 }
 alert (`Your score is ${userScore} and the computer's score is ${computerScore}`)
 
+gamesPlayed++;
 playAgain = confirm("Do you want to play again?")
-console.log(playAgain)
 
 }
 
+
+
 alert(`Thank you for playing! Your final score is ${userScore} and the computer's final score ${computerScore}`)
-
-
-//Add an alert to declare what move the computer made
+alert(`You played ${gamesPlayed} games, you lost ${gamesLost} games, you won ${gamesWon} games and you tied ${gamesTied} games`)
